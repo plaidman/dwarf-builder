c67e89597383df5df1bcac88fb06c299a0c90445
plaidman <jrtomsic@gmail.com>
2012-04-23 16:45:47 -0400

    started laying out the cocoa app

---
+++ cocoa/DwarfBuilder/DwarfBuilder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DwarfBuilder
+//
+//  Created by Tomsic, Jason on 4/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
