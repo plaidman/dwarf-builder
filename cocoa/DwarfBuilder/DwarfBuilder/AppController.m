b4eb1b8cba15188338be94f3b2b207c449171213
plaidman <jrtomsic@gmail.com>
2012-04-24 21:24:48 -0400

    cocoa

---
+++ cocoa/DwarfBuilder/DwarfBuilder/AppController.m
@@ -0,0 +1,65 @@
+//
+//  AppController.m
+//  DwarfBuilder
+//
+//  Created by Jason Tomsic on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppController.h"
+
+@implementation AppController
+
+/* VISUAL SETTINGS */
+@synthesize windowWidthField;
+@synthesize windowHeightField;
+@synthesize fullscreenCheckbox;
+
+@synthesize tilesetRadio;
+@synthesize creatureCheckbox;
+
+@synthesize introCheckbox;
+@synthesize fPSCheckbox;
+@synthesize liquidDepthCheckbox;
+@synthesize idlersPopup;
+
+@synthesize fontRadio;
+@synthesize fontCheckbox;
+
+/* APPLICATION SETTINGS */
+@synthesize soundCheckbox;
+@synthesize extendOSTCheckbox;
+@synthesize volumeSlider;
+
+@synthesize autosaveRadio;
+@synthesize compressSavesCheckbox;
+@synthesize pauseOnLoadCheckbox;
+@synthesize autoBackupSavesCheckbox;
+@synthesize pauseOnSaveCheckbox;
+
+@synthesize cFPSCapField;
+@synthesize gFPSCapField;
+
+@synthesize keybindingsRadio;
+
+/* GAMEPLAY SETTINGS */
+@synthesize skillRustCheckbox;
+@synthesize embarkWarningCheckbox;
+@synthesize grazingAnimalCheckbox;
+@synthesize pauseCaveinCheckbox;
+@synthesize extraShellCheckbox;
+@synthesize pauseWarmDampCheckbox;
+@synthesize temperatureCheckbox;
+@synthesize aquiferCheckbox;
+@synthesize caveinCheckbox;
+@synthesize invaderCheckbox;
+@synthesize weatherCheckbox;
+
+@synthesize dwarfCapField;
+@synthesize childHardCapField;
+@synthesize childPercentCapField;
+
+@synthesize embarkWidthField;
+@synthesize embarkHeightField;
+
+@end
